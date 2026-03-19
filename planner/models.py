75dddc28646415079f8196dbf33724623e0e1afd
Omnisslayer01 <abduldiyandeshmukh2005@gmail.com>
2026-03-19 23:43:09 +0530

    added function __str__ to models

--- planner/models.py
+++ planner/models.py
@@ -2,15 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class User_state(models.Model):
-    user = models.OneToOneField(User , on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     current_energy_level=models.IntegerField()
     last_assessed=models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'{self.user} has {self.current_energy_level} and was last assessed at {self.last_assessed}'
+
 class Vault_Goal(models.Model):
-    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=64)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} is active= {self.is_active} and was created at {self.created_at}'
 
 class Micro_task(models.Model):
     parent_goal = models.ForeignKey(Vault_Goal , on_delete=models.CASCADE)
@@ -25,6 +31,9 @@ class Micro_task(models.Model):
     ]
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='vaulted')
     soft_deadline = models.DateTimeField(null=True , blank= True)
+
+    def __str__(self):
+        return f'{self.title} has {self.energy_cost} and has status= {self.status} having deadline {self.soft_deadline}'
 
 
 
