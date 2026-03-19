a48c6143b3c985929d348ef8c27d7d5a41ad80aa
Omnisslayer01 <abduldiyandeshmukh2005@gmail.com>
2026-03-19 22:45:12 +0530

    Making changes to model

--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class User_state():
+    #user=models.OneToOneField()
+    current_energy_level=models.IntegerField(max_length=10)
+    last_assessed=models.DateTimeField(auto_now=True)
+
+class Vault_Goal():
