c07f9b2006efad0bb86ac5f8341d034a8e408079
yashthorat <djrobber07@gmail.com>
2026-03-19 23:14:59 +0530

    wrote models.py and admin.py

--- planner/admin.py
+++ planner/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import User_state , Vault_Goal , Micro_task
 # Register your models here.
+admin.site.register(User_state)
+admin.site.register(Vault_Goal)
+admin.site.register(Micro_task)
--- planner/models.py
+++ planner/models.py
@@ -1,9 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class User_state():
-    #user=models.OneToOneField()
-    current_energy_level=models.IntegerField(max_length=10)
+class User_state(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
+    current_energy_level=models.IntegerField()
     last_assessed=models.DateTimeField(auto_now=True)
 
-class Vault_Goal():
+class Vault_Goal(models.Model):
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Micro_task(models.Model):
+    parent_goal = models.ForeignKey(Vault_Goal , on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    energy_cost = models.IntegerField()
+    skip_count = models.IntegerField(default=0)
+    STATUS_CHOICES =[
+    ('vaulted', 'Vaulted'),
+    ('flashlight', 'Flashlight'),
+    ('completed', 'Completed'),
+    ('intervention', 'Intervention')
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='vaulted')
+    soft_deadline = models.DateTimeField(null=True , blank= True)
+
+
+
+    
+
