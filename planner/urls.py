2468404a886f9129669d92194d0aa8befbd5600c
Omnisslayer01 <abduldiyandeshmukh2005@gmail.com>
2026-04-08 20:41:43 +0530

    better url flexibility

--- planner/urls.py
+++ planner/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/",views.flashlight_tasks, name="home"),
+    path("",views.flashlight_tasks, name="home"),
     path("update_task/" , views.update_task , name = "update_task"),
     path("waifu/", views.waifu_chat , name = "user_response"),
 
